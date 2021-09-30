f97a4bf9fc691c013b68e9a27062249f90f95815
penz6 <thetechkid66@gmail.com>
2021-09-29 21:57:43 -0400

    added more advanced functions

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+print('Currently Supports Five Numbers')
+og1 = float(raw_input('Put in the first original number \n'))
+og2 = input('Put in the second original number \n')
+og3 = input('Put in the third original number \n')
+og4 = input('Put in the fourth original number \n')
+og5 = input('Put in the fifth original number \n')
+out1 = float(raw_input('Put in the first number you got out \n'))
+out2 = input('Put in the second number you got out \n')
+out3 = input('Put in the third number you got out \n')
+out4 = input('Put in the fourth number you got out \n')
+out5 = input('Put in the fifth number you got out \n')
 f=0
-while f <= 10:
-    f += 1
-    sum1 = f*1
-    sum2 = f*5
-    sum3 = f*12
-    sum4 = f*14
-    sum4 = f*15
-    sum5 = f*23
-    print(sum1,sum2,sum3,sum4,sum5)
+while og1 != out1 and og2 != out2 and og3 != out3 and og4 != out4 and og5 != out5:
+    f += .01
+    sum1 = out1*f
+    sum2 = out2*f
+    sum3 = out3*f
+    sum4 = out4*f
+    sum5 = out5*f
+    print(f'is your number')
 
 
